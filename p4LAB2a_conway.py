5ff6e3518f4ba7637cff134f4657b0c54c3fe3f1
chromeguardsmen <103205831+chromeguardsmen@users.noreply.github.com>
2022-05-10 12:28:33 -0400

    Add files via upload

---
+++ p4LAB2a_conway.py
@@ -0,0 +1,16 @@
+import turtle
+turtle.forward(90)
+turtle.left(90)
+turtle.forward(90)
+turtle.left(90)
+turtle.forward(90)
+turtle.left(90)
+turtle.forward(90)
+turtle.done
+import turtle
+turtle.forward(100) 
+turtle.left(120)
+turtle.forward(100)
+turtle.left(120)
+turtle.forward(100)
+turtle.done
